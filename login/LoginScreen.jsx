04147d953e0aa2bfeb1f8465a0f1f64614755923
Lycah Dinglasan <lycah.dinglasan@gmail.com>
2025-04-05 18:42:57 +0800

    Create LoginScreen.jsx

---
+++ login/LoginScreen.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+import { Logo, WelcomeText, LoginForm, FooterText } from './LoginComponents';
+
+const LoginScreen = () => {
+  return (
+    <View style={styles.container}>
+      <View style={styles.loginCard}>
+        <Logo />
+        <WelcomeText />
+        <LoginForm />
+        <FooterText />
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    width: '100%',
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 20,
+  },
+  loginCard: {
+    width: 347,
+    borderRadius: 40,
+    paddingTop: 40,
+    paddingHorizontal: 30,
+    paddingBottom: 40,
+    alignItems: 'center',
+    backgroundColor: '#FFFFFF',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+  },
+});
+
+export default LoginScreen;
