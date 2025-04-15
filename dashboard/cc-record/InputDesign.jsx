1af39ec4beb1014baa4be182c8dd3aface4eb303
gems <76839968+gems29@users.noreply.github.com>
2025-04-15 20:03:11 +0800

    Update InputDesign.jsx

--- dashboard/cc-record/InputDesign.jsx
+++ dashboard/cc-record/InputDesign.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { View, StyleSheet } from "react-native";
+import { View, StyleSheet, ScrollView, Dimensions } from "react-native";
 import TimeframeSelector from "./TimeframeSelector";
 import AmountDisplay from "./AmountDisplay";
 import TransactionList from "./TransactionList";
@@ -9,42 +9,55 @@ const InputDesign = () => {
   const [selectedTimeframe, setSelectedTimeframe] = useState("Monthly");
 
   return (
-    <View style={styles.container}>
-      <TimeframeSelector
-        selected={selectedTimeframe}
-        onSelect={setSelectedTimeframe}
-      />
+    <ScrollView contentContainerStyle={styles.scrollContainer}>
+      <View style={styles.container}>
+        <TimeframeSelector
+          selected={selectedTimeframe}
+          onSelect={setSelectedTimeframe}
+        />
 
-      <View style={styles.datePickerContainer}>
-        <DateFilterIcon />
-      </View>
+        <View style={styles.datePickerContainer}>
+          <DateFilterIcon />
+        </View>
 
-      <View style={styles.contentContainer}>
-        <AmountDisplay amount="2,005.00" trend="increase" />
-        <TransactionList />
+        <View style={styles.contentContainer}>
+          <AmountDisplay amount="2,005.00" trend="increase" />
+          <TransactionList />
+        </View>
       </View>
-    </View>
+    </ScrollView>
   );
 };
 
+const { width } = Dimensions.get("window");
+
 const styles = StyleSheet.create({
+  scrollContainer: {
+      flexGrow: 1,
+      backgroundColor: "#212124",
+      padding: 16,
+      paddingTop: 50,
+    },
+
   container: {
     display: "flex",
+    backgroundColor: "#2A2A2E",
     flexDirection: "column",
-    width: 358,
+    width: 325,
     height: 544,
     borderRadius: 9,
-    borderWidth: 3,
-    borderColor: "#D38462",
+    paddingTop: 32,
   },
   datePickerContainer: {
     display: "flex",
     justifyContent: "center",
     alignItems: "center",
     marginTop: 34,
+
   },
   contentContainer: {
     paddingHorizontal: 29,
+
   },
 });
 
