1871a2c9edb4d386ba2512b77bb9cd96585582fc
Lycah Dinglasan <lycah.dinglasan@gmail.com>
2025-05-09 22:44:06 +0800

    Create Divider.jsx

---
+++ dashboard/hamburger/Divider.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+
+const Divider = () => {
+  return <View style={styles.divider} />;
+};
+
+const styles = StyleSheet.create({
+  divider: {
+    height: 1,
+    backgroundColor: '#2F2E34',
+    marginTop: 24,
+    marginBottom: 22,
+  },
+});
+
+export default Divider;
