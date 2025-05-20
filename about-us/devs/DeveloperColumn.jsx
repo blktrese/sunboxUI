9b06ef73a6aae6c9716b6f54a553928945cebf78
Lycah Dinglasan <lycah.dinglasan@gmail.com>
2025-05-20 11:46:57 +0800

    Create DeveloperColumn.jsx

---
+++ about-us/devs/DeveloperColumn.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const DeveloperColumn = ({ names, color }) => {
+  return (
+    <View style={styles.container}>
+      {names.map((name, index) => (
+        <Text key={index} style={[styles.text, { color }]}>
+          {name}
+        </Text>
+      ))}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  text: {
+    fontSize: 12,
+    fontWeight: '600',
+    lineHeight: 17,
+  },
+});
+
+export default DeveloperColumn;
