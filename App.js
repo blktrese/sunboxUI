dba2a93c31445bd079c5ce84d87ae0c7fc551d32
gems <76839968+gems29@users.noreply.github.com>
2025-05-23 00:48:01 +0800

    Update App.js

--- App.js
+++ App.js
@@ -5,19 +5,21 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import LoginScreen from './login/LoginScreen';
 import LoginError from './loginerror/LoginError';
 import Dashboard from './dashboard/ccrecord/InputDesign';
+import SplashScreen from './SplashScreen';
 import { View, StyleSheet } from 'react-native';
 
 const Stack = createNativeStackNavigator();
 
-export default function App() {  return (
+export default function App() {
+  return (
     <NavigationContainer>
       <View style={styles.appContainer}>
-        <Stack.Navigator initialRouteName="Login" screenOptions={{ headerShown: false }}>
+        <Stack.Navigator initialRouteName="Splash" screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="Splash" component={SplashScreen} />
           <Stack.Screen name="Login" component={LoginScreen} />
           <Stack.Screen name="LoginError" component={LoginError} />
           <Stack.Screen name="Dashboard" component={Dashboard} />
-          <Stack.Screen name="Home" component={Dashboard} />
-          <Stack.Screen name="About" component={Dashboard} />
+          <Stack.Screen name="AboutUs" component={AboutScreen} />
         </Stack.Navigator>
       </View>
     </NavigationContainer>
@@ -28,5 +30,12 @@ const styles = StyleSheet.create({
   appContainer: {
     flex: 1,
     backgroundColor: "#212124",
+  },
+      mainContainer: {
+    flex: 1,
+  },
+  contentContainer: {
+    flex: 1,
+    padding: 24,
   },
 });
